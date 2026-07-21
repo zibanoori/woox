451eae7653996a46be03c6431b0536822353d34a
zibanoori <noorizibaa@gmail.com>
2026-07-21 20:50:40 +0330

    add html codes in template and settings

--- woox/settings.py
+++ woox/settings.py
@@ -99,4 +99,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
