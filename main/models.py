75d4b91b4f2b24887f6a09cad98456993f8400bd
zibanoori <noorizibaa@gmail.com>
2026-07-22 20:42:09 +0330

    add image field in models

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class core(models.Model):
-    title = models.charField(max_length=100, blank=True)
-    website_title = models.charField(max_length=100, blank=True)
+    title = models.charFieldharField(max_length=100, blank=True)
+    website_title = models.CharField(max_length=100, blank=True)
+    logo = models.ImageFiel(upload_to="core/" ,blank=True)
 
