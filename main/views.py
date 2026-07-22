ee467e13835743f12f3fd532b305423ca69fd087
zibanoori <noorizibaa@gmail.com>
2026-07-22 22:28:16 +0330

    add base_details (Core) to home view context

--- main/views.py
+++ main/views.py
@@ -3,4 +3,8 @@ from .models import Core
 
 
 def index(request):
+    base_details = Core.objects.all().first()
+    context = {
+        "Core": base_details
+    }
     return render(request,"index.html")
