4302dd2c915474abd14e687135ecb7f11ee7a18d
zibanoori <noorizibaa@gmail.com>
2026-07-23 00:25:59 +0330

    import RichTextField in models

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Core(models.Model):
     title = models.CharField(max_length=100, blank=True)
     website_title = models.CharField(max_length=100, blank=True)
     logo = models.ImageField(upload_to="core/", blank=True)
     favicon = models.ImageField(upload_to="core/", blank=True)
-    copyright = models.TextField(blank=True)
+    copyright = RichTextField(blank=True)
     
     def __str__(self):
         return "Core Details"
