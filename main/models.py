cf8bb1537f6673a469f4ecfd55c696483fdb5def
zibanoori <noorizibaa@gmail.com>
2026-07-22 20:58:51 +0330

    add __str__ method to Core model for admin display

--- main/models.py
+++ main/models.py
@@ -4,3 +4,7 @@ class core(models.Model):
     title = models.CharField(max_length=100, blank=True)
     website_title = models.CharField(max_length=100, blank=True)
     logo = models.ImageField(upload_to="core/", blank=True)
+    copyright = models.TextField(blank=True)
+    
+    def __str__(self):
+        return "Core Details"
