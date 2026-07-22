56139fab57ace641e371c2903a56dfc6d7c8f364
zibanoori <noorizibaa@gmail.com>
2026-07-22 21:18:33 +0330

    add mixin to prevent deletion in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Core
 
+class MoDeleteAdminMixin:
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(MyModel)
+class MyModelAdmin(admin.ModelAdmin, MoDeleteAdminMixin):
+    def has_add_permission(self, request):
+        return not MyModel.objects.exists()
+
+
 admin.site.register(Core)
 
 
