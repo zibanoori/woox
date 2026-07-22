d138ad1a6bce4f98a74e7e8043c91cc43d242471
zibanoori <noorizibaa@gmail.com>
2026-07-22 20:53:47 +0330

    add pillow for image field and debug models

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class core(models.Model):
-    title = models.charFieldharField(max_length=100, blank=True)
+    title = models.CharField(max_length=100, blank=True)
     website_title = models.CharField(max_length=100, blank=True)
-    logo = models.ImageFiel(upload_to="core/" ,blank=True)
-
+    logo = models.ImageField(upload_to="core/", blank=True)
