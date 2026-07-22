150027197ea9e80a6cc9eb9db00062b9224030d5
zibanoori <noorizibaa@gmail.com>
2026-07-22 20:10:06 +0330

    writing a title model and website_title model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class core(models.Model):
+    title = models.charField(max_length=100, blank=True)
+    website_title = models.charField(max_length=100, blank=True)
 
